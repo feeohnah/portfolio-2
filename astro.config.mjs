f9c93583547be3f90e3334a476c7d9bc603dbbb7
feeohnah <fiona@studiocartogram.com>
2025-10-24 14:20:45 +0200

    removed vercel package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({
-  // ...
-  output: 'static',
-  adapter: vercel({
-    imageService: true,
-  }),
-});
+export default defineConfig({});
